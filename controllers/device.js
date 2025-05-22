2d947821ca7abb561a0b0638c7ffc24ba151a104
Jessica1623 <msdelrosario3@up.edu.ph>
2025-05-22 04:52:55 -0800

    Update device.js

--- controllers/device.js
+++ controllers/device.js
@@ -16,7 +16,7 @@ const isDuringCurfew = (date) => {
   const hours = phDate.getHours();
   const minutes = phDate.getMinutes();
   
-  return (hours > 19) || (hours < 6);
+  return (hours > 22) || (hours < 6);
 };
 
 // Check if student has valid permit for current time
